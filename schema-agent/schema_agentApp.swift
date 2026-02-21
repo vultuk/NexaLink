bb415e230e4f8b868ac2821fc8181f0ade033d89
Simon Skinner <talkto@simonskinner.me>
2026-02-21 18:22:47 +0000

    First Commit

---
+++ schema-agent/schema_agentApp.swift
@@ -0,0 +1,17 @@
+//
+//  schema_agentApp.swift
+//  schema-agent
+//
+//  Created by Simon Skinner on 21/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct schema_agentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
